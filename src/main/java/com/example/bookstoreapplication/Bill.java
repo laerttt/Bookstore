a2469a1defe2399b1c0b7fc087329e7b37452f0e
Laert Huti <la2002ert@gmail.com>
2023-01-31 22:32:38 +0100

    Bill Class Complete

---
+++ src/main/java/com/example/bookstoreapplication/Bill.java
@@ -0,0 +1,42 @@
+package com.example.bookstoreapplication;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Bill {
+    public static int billNumber;
+    private int numberOfBooks;
+    private ArrayList<String> books = new ArrayList<String>();
+    private int totalPrice=0;
+    private Date date;
+    Bill( ArrayList<String> books, Date date){
+        this.numberOfBooks = books.size();
+        this.date = date;
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder bill = new StringBuilder();
+        bill.append("BillNo: ");
+        bill.append(billNumber);
+        bill.append("\n");
+        bill.append("Data: ");
+        bill.append(this.date);
+        bill.append("\n");
+        for(int i=0;i<this.books.size();i++){
+            bill.append(books.get(i));
+            bill.append("\t");
+            i++;
+            bill.append(books.get(i));
+            bill.append("L");
+            bill.append("\n");
+            this.totalPrice+= Integer.parseInt(books.get(i));
+        }
+        bill.append("Totali:\t");
+        bill.append(this.totalPrice);
+        bill.append("L");
+        return bill.toString();
+    }
+}
+
--- src/main/java/com/example/bookstoreapplication/Main.java
+++ src/main/java/com/example/bookstoreapplication/Main.java
@@ -3,6 +3,7 @@ package com.example.bookstoreapplication;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main extends Application {
@@ -13,6 +14,19 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         Date date = new Date();
-        Person laert = new Librarian("Laert", "Huti", date,4);
+//        Person laert = new Librarian("Laert", "Huti", date,4);
+
+        ArrayList<String> test = new ArrayList<>();
+        test.add("Përse me tërheq seksi i njëjtë\nAutori: Kelvin Gjikola");
+        test.add("3000");
+        test.add("Boli dhe sufllaqja. Si të ushqehemi shëndetshëm?\nAutori: Kelvin Gjikola");
+        test.add("2000");
+        test.add("Java coding dhe sufllaqet e Oles\nAutori: Kelvin Gjikola");
+        test.add("4500");
+        test.add("Ndihmë u bera gej\nAutori: Kelvin Gjikola");
+        test.add("2250");
+        Bill bill = new Bill(test, date);
+        System.out.print(bill.toString());
+
     }
 }
