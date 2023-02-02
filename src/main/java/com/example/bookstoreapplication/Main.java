f5f08b3ac7993c7f1e29dee7ef7f93b87da506c8
Laert Huti <la2002ert@gmail.com>
2023-02-02 23:26:47 +0100

    nothing important

--- src/main/java/com/example/bookstoreapplication/Main.java
+++ src/main/java/com/example/bookstoreapplication/Main.java
@@ -15,24 +15,6 @@ public class Main extends Application {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Date date = new Date();
-        Person laert = new Librarian("Laert", "Huti", date,4);
-        laert.setUserName("laertHuti");
-        laert.setPassword("123");
-        Person kelvin = new Manager("kelvin", "gjik", date);
-        kelvin.setUserName("kelvingj");
-        kelvin.setPassword("123");
-        Person ang = new Administrator("angel", "l", date);
-        ang.setUserName("angell");
-        ang.setPassword("123");
-        ArrayList<Person> employee = new ArrayList<>();
-        employee.add(laert);
-        employee.add(kelvin);
-        employee.add(ang);
-        try(ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("Employee.dat"))
-        ){
-            for (Person person : employee)
-                output.writeObject(person);
-        }
-        System.out.println(LogInControls.checkLogIn("laetHuti","123"));
+
     }
 }
