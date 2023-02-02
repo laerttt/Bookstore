51752c1f4849a5835de9c5156cb336a151418de8
Laert Huti <la2002ert@gmail.com>
2023-02-02 23:21:48 +0100

    Fixed some issues on Bill class

--- src/main/java/com/example/bookstoreapplication/Bill.java
+++ src/main/java/com/example/bookstoreapplication/Bill.java
@@ -10,22 +10,34 @@ public class Bill implements Serializable {
     private ArrayList<String> books = new ArrayList<String>();
     private int totalPrice=0;
     private Date date;
+    private int librarianID;
     Bill(){
 
     }
-    Bill( ArrayList<String> books, Date date){
+
+    /**
+     * Enter librarian ID books and date to print the order bill
+     * @param books;
+     * @param date;
+     * @param librarianID;
+     */
+    Bill( ArrayList<String> books, Date date,int librarianID){
         this.numberOfBooks = books.size();
         this.date = date;
         this.books = books;
+        this.librarianID = librarianID;
     }
 
     @Override
     public String toString() {
         StringBuilder bill = new StringBuilder();
+        bill.append("Librarian No: ");
+        bill.append(librarianID);
+        bill.append("\n");
         bill.append("BillNo: ");
         bill.append(billNumber);
         bill.append("\n");
-        bill.append("Data: ");
+        bill.append("Date: ");
         bill.append(this.date);
         bill.append("\n");
         for(int i=0;i<this.books.size();i++){
@@ -37,7 +49,7 @@ public class Bill implements Serializable {
             bill.append("\n");
             this.totalPrice+= Integer.parseInt(books.get(i));
         }
-        bill.append("Totali:\t");
+        bill.append("Total:\t");
         bill.append(this.totalPrice);
         bill.append("L");
         return bill.toString();
